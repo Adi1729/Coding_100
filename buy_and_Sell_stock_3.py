193c0d021632a6cab580118030ea5c85f4057874
Aditya Yadav <adiyadav.1729@gmail.com>
2018-07-30 23:59:45 +0530

    Day 4
    Time limit exceeded for 1 test case

---
+++ buy_and_Sell_stock_3.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+'''
+Example 1:
+
+Input: [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+             Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+Example 2:
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+'''
+
+class Solution(object):
+    
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        if len(prices)==0:
+            return 0
+
+        
+        def cal_pr(prices):
+
+            max_profit=0
+            min_price=float('inf')
+            i=0
+            while(i<len(prices)):
+
+                if(prices[i]<min_price):
+                    min_price=prices[i]
+                elif(prices[i]-min_price>max_profit):
+                    max_profit=prices[i]-min_price
+
+                i=i+1
+
+            return max_profit
+
+
+        left_profit_list=[]
+        right_profit_list=[]
+        profit=[]
+
+        for i in range(0,len(prices)):
+            left_profit=cal_pr(prices[0:i])
+            right_profit=cal_pr(prices[i:])
+            profit.append(left_profit+right_profit)
+
+
+        return max(profit)
